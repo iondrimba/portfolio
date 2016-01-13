41be587289630eabc870bf88f397994c8a460667
Ion D. FIlho <iondrimba@hotmail.com>
2016-01-13 21:17:18 -0200

    Refactored DOM manipulation with noJquery on Home view

--- source/scripts/app/views/home.js
+++ source/scripts/app/views/home.js
@@ -1,58 +1,66 @@
 ﻿/* global Detector */
-define(['lib/NoJQuery', 'views/grid3d'], function (NoJQuery, Grid3D) {
-    var Home = function (app) {
+define(['noJquery', 'views/grid3d'], function(NoJQuery, Grid3D) {
+    var Home = function(app) {
         this.el = '.home';
-        this.njq = NoJQuery;
+        this.$$ = NoJQuery;
         this.completed = false;
         this.menu = app.menu;
 
-        this.initialize = function () {
-            
-            this.njq.removeClass(this.njq.select('body'), 'body-gradient');
-            this.njq.addClass(this.njq.select(this.el), 'body-gradient');            
-            this.$el = this.njq.select(this.el);
-            
+        this.initialize = function() {
+
+            this.$$('body').removeClass('body-gradient');
+            this.$$(this.el).addClass('body-gradient');
+            this.$el = this.$$(this.el);
+
+            //INIT WEBGL GRID ONLY IDF SUPPORTED
             if (Detector.webgl) {
-                this.grid3D = new Grid3D();              
-            } 
-            
-            this.njq.addClass(this.njq.select('.loading-arrow'), 'hidden');
+                this.grid3D = new Grid3D();
+            }
+
+            //HIDE LOADER
+            this.$$('.loading-arrow').addClass('hidden');
         };
-        this.execute = function () {
+        this.execute = function() {
             if (Detector.webgl) {
-                 if (this.grid3D.executed == false) {
+                if (this.grid3D.executed == false) {
                     this.grid3D.execute();
                 }
             }
-            
-            //app.showConsoleGretings();
-                          
-            this.njq.removeClass(this.$el, 'hidden');
+
+            //app.showConsoleGretings();                          
+
+            //SHOW VIEW
+            this.$el.removeClass('hidden');
+
+            //OPEN FULL MODE
             this.full();
+
             app.event.publish('completed');
         };
 
-        this.full = function () {
-            this.njq.removeClass(this.$el, 'show-min');
+        this.full = function() {
 
-            this.njq.addClass(this.$el, 'show-full', true);
+            this.$el.removeClass('show-min');
 
-            this.njq.removeClass(this.njq.select('.scroll-down-button'), 'hidden');
-            this.njq.removeClass(this.njq.select('body'), 'show-scroll');
-            this.njq.addClass(this.njq.select('.scroll-down-button'), 'draw-in');
+            //ANIMATE FULL VIEW
+            this.$el.addClass('show-full');
+
+            this.$$('.scroll-down-button').removeClass('hidden');
+            this.$$('body').removeClass('show-scroll');
+            this.$$('.scroll-down-button').addClass('draw-in');
             this.completed = true;
 
             this.menu.hide();
         };
-        this.minimize = function () {
-            this.njq.removeClass(this.$el, 'show-full');
+        this.minimize = function() {
+            this.$el.removeClass('show-full');
 
-            this.njq.addClass(this.$el, 'show-min', true);
+            //ANIMATE MINIMIZED VIEW
+            this.$el.addClass('show-min');
 
-            this.njq.addClass(this.njq.select('.scroll-down-button'), 'hidden');
-            this.njq.addClass(this.njq.select('body'), 'show-scroll');
-            this.njq.redraw(this.njq.select('.scroll-down-button')[0]);
-            this.njq.removeClass(this.njq.select('.scroll-down-button'), 'draw-in');
+            this.$$('.scroll-down-button').addClass('hidden');
+            this.$$('body').addClass('show-scroll');
+            this.$$('.scroll-down-button').removeClass('draw-in');
             this.completed = true;
 
             this.menu.animate();
