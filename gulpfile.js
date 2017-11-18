3b148a102a09a7accca4943ed14cad7050a38a41
Ion D. Filho <iondrimba@gmail.com>
2017-11-18 13:11:01 -0200

    minified build

--- gulpfile.js
+++ gulpfile.js
@@ -13,60 +13,60 @@ var minor = semver.inc(pckg.version, 'minor');
 var major = semver.inc(pckg.version, 'major');
 
 var requirePaths = {
-    noJquery: 'node_modules/nojquery/nojquery',
-    TweenLite: 'src/scripts/vendors/TweenLite',
-    handlebars: 'node_modules/handlebars/dist/handlebars',
-    notifyCss: 'src/scripts/app/core/notifyCss',
-    page: 'node_modules/page/page',
-    models: 'src/scripts/app/models',
-    views: 'src/scripts/app/views',
-    vendors: 'src/scripts/vendors',
-    core: 'src/scripts/app/core'
+  noJquery: 'node_modules/nojquery/nojquery',
+  TweenLite: 'src/scripts/vendors/TweenLite',
+  handlebars: 'node_modules/handlebars/dist/handlebars',
+  notifyCss: 'src/scripts/app/core/notifyCss',
+  page: 'node_modules/page/page',
+  models: 'src/scripts/app/models',
+  views: 'src/scripts/app/views',
+  vendors: 'src/scripts/vendors',
+  core: 'src/scripts/app/core'
 };
 
 function bumpPackageJson(type) {
-    return gulp.src(['./package.json'])
-        .pipe(bump({
-            version: type
-        }))
-        .pipe(gulp.dest('./'));
+  return gulp.src(['./package.json'])
+    .pipe(bump({
+      version: type
+    }))
+    .pipe(gulp.dest('./'));
 }
 
 function bumpAppFiles(version) {
-    var options = {};
-    options.version = version;
-    options.indexFile = './public/index.html';
+  var options = {};
+  options.version = version;
+  options.indexFile = './public/index.html';
 
-    options.filePath = ['./public/scripts/main.js'];
-    options.outputfolder = ['./public/scripts/'];
+  options.filePath = ['./public/scripts/main.js'];
+  options.outputfolder = ['./public/scripts/'];
 
-    renameMe(options);
+  renameMe(options);
 }
 
 
 function injectFiles() {
-    var options = {};
+  var options = {};
 
-    options.cssPath = './public/css/main.css';
-    options.jsPath = './public/sw-setup.js';
-    options.indexFile = './public/index.html';
+  options.cssPath = './public/css/main.css';
+  options.jsPath = './public/sw-setup.js';
+  options.indexFile = './public/index.html';
 
-    var injectedHtml = injectMe(options);
+  var injectedHtml = injectMe(options);
 }
 
 //PATCH
 gulp.task('patch', function () {
-    return bumpPackageJson(patch);
+  return bumpPackageJson(patch);
 });
 
 //MINOR
 gulp.task('minor', function () {
-    return bumpPackageJson(minor);
+  return bumpPackageJson(minor);
 });
 
 //MAJOR
 gulp.task('major', function () {
-    return bumpPackageJson(major);
+  return bumpPackageJson(major);
 });
 
 //clean public files
@@ -92,12 +92,12 @@ gulp.task('minifyjs', require('./tasks/minifyjs.js'));
 
 //require dev
 gulp.task('requirejs:dev', function () {
-    return require('./tasks/requirejs.js')(requirePaths, 'none');
+  return require('./tasks/requirejs.js')(requirePaths, 'none');
 });
 
 //require production
 gulp.task('requirejs:prod', function () {
-    return require('./tasks/requirejs.js')(requirePaths, 'none');
+  return require('./tasks/requirejs.js')(requirePaths, 'none');
 });
 
 //browsersync task
@@ -105,39 +105,39 @@ gulp.task('browser-sync', require('./tasks/browser-sync.js'));
 
 //offline support
 gulp.task('service-worker', function (callback) {
-    var rootDir = 'public';
-    swPrecache.write(path.join(rootDir, 'service-worker.js'), {
-        staticFileGlobs: [
-            rootDir + '/**/*.{html,json}',
-            rootDir + '/**/require.js',
-            rootDir + '/**/main.1.40.0.{js,css}',
-            rootDir + '/*.{png,jpg,gif}',
-            rootDir + '/**/*.{png,jpg,gif}',
-            rootDir + '/**/*.{svg,eot,ttf,woff,woff2}'
-        ],
-        stripPrefix: rootDir,
-        cacheId: pckg.version,
-        runtimeCaching: [{
-            urlPattern: '(.*)',
-            handler: 'cacheFirst'
-        }]
-    }, callback);
+  var rootDir = 'public';
+  swPrecache.write(path.join(rootDir, 'service-worker.js'), {
+    staticFileGlobs: [
+      rootDir + '/**/*.{html,json}',
+      rootDir + '/**/require.js',
+      rootDir + '/**/main.1.40.0.{js,css}',
+      rootDir + '/*.{png,jpg,gif}',
+      rootDir + '/**/*.{png,jpg,gif}',
+      rootDir + '/**/*.{svg,eot,ttf,woff,woff2}'
+    ],
+    stripPrefix: rootDir,
+    cacheId: pckg.version,
+    runtimeCaching: [{
+      urlPattern: '(.*)',
+      handler: 'cacheFirst'
+    }]
+  }, callback);
 });
 
 gulp.task('bump-patch', ['patch', 'service-worker'], function renamePatch() {
-    bumpAppFiles(patch);
+  bumpAppFiles(patch);
 });
 
 gulp.task('bump-minor', ['minor', 'service-worker'], function renameMinor() {
-    bumpAppFiles(minor);
+  bumpAppFiles(minor);
 });
 
 gulp.task('bump-major', ['major', 'service-worker'], function renameMajor() {
-    bumpAppFiles(major);
+  bumpAppFiles(major);
 });
 
 gulp.task('inject', function inject() {
-    injectFiles();
+  injectFiles();
 });
 
 //watch task
@@ -145,5 +145,5 @@ gulp.task('watch', require('./tasks/watch.js'));
 
 gulp.task('default', gulpSequence(['eslint', 'scss-lint', 'sass', 'copy', 'requirejs:dev'], 'inject', 'browser-sync', 'watch'));
 
-gulp.task('prod', gulpSequence(['eslint', 'scss-lint', 'sass', 'cssmin', 'requirejs:prod','clean'], 'copy', 'bump-minor', 'inject'));
+gulp.task('prod', gulpSequence(['eslint', 'sass', 'cssmin', 'requirejs:prod', 'clean'], 'copy', 'minifyjs', 'bump-minor', 'inject'));
 
--- tasks/minifyjs.js
+++ tasks/minifyjs.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 
 module.exports = function () {
-    return gulp.src('./public/scripts/require.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('./public/scripts'));
+  return gulp.src('./public/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/scripts'));
 };
