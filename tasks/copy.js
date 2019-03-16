47e4e45b2193fa534ba03fab2943997dd38b3533
Ion D. Filho <iondrimba@gmail.com>
2019-03-16 13:03:00 -0700

    fix path

--- tasks/copy.js
+++ tasks/copy.js
@@ -4,6 +4,6 @@ var concat = require('gulp-concat');
 module.exports = function () {
   gulp.src(['src/scripts/vendors/require.js'])
     .pipe(gulp.dest('./public/scripts/'));
-  gulp.src(['manifest.json', './src/templates/index.html', './src/scripts/vendors/sw-setup.js'])
+  gulp.src(['manifest.json', './src/templates/index.html', './src/scripts/vendors/sw-setup.js', './src/scripts/vendors/three.min.js'])
     .pipe(gulp.dest('./public/'));
 };
