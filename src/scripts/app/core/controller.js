25c522828d2c908344e5144dff9b001cabe7a406
Ion D. Filho <iondrimba@gmail.com>
2019-03-16 12:40:48 -0700

    debug

--- src/scripts/app/core/controller.js
+++ src/scripts/app/core/controller.js
@@ -56,7 +56,7 @@ define(['page', 'views/home', 'views/about'], function (page, Home, About) {
     };
 
     this.onHome = function (ctx, next) {
-      console.log(this)
+      console.log(this.home)
       this.home.execute();
     };
     this.onAbout = function (ctx, next) {
